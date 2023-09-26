65e50654dc5c75a833c470559cce2630d7a46c34
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2023-09-26 14:18:17 +0300

    Create edge case tests for pool splitting
    Edge cases include:
    - a predicate that returns only true
    - a predicate that returns only false
    - [!] a predicate that returns only null
    
    WARNING - the split pool with an only
    null predicate times out due to an infinite
    loop that is occuring.

--- test/unit/pool.test.ts
+++ test/unit/pool.test.ts
@@ -1,6 +1,7 @@
 import { getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
 import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
 import { fromB64 } from '@mysten/sui.js/utils';
+import { SuiObjectRef } from '@mysten/sui.js/src/types/objects';
 
 import { Pool } from '../../src';
 
@@ -11,22 +12,21 @@ const TEST_KEYS = [
   'flash leave dilemma swing lab flavor shoot civil rookie list gather soul', // mnemonic
 ];
 
-const ADMIN_SECRET_KEY = process.env.ADMIN_SECRET_KEY;
+const ADMIN_SECRET_KEY = TEST_KEYS[0];
+const adminPrivateKeyArray = Uint8Array.from(
+  Array.from(fromB64(ADMIN_SECRET_KEY!)),
+);
+const adminKeypair = Ed25519Keypair.fromSecretKey(
+  adminPrivateKeyArray.slice(1),
+);
+const client = new SuiClient({
+  url: getFullnodeUrl('testnet'),
+});
+
 
 describe('Pool creation with factory', () => {
-  const chunksOfGas = 2;
-  const txnsEstimate = 10;
-
-  const adminPrivateKeyArray = Uint8Array.from(
-    Array.from(fromB64(ADMIN_SECRET_KEY!)),
-  );
-  const adminKeypair = Ed25519Keypair.fromSecretKey(
-    adminPrivateKeyArray.slice(1),
-  );
-
-  const client = new SuiClient({
-    url: getFullnodeUrl('testnet'),
-  });
+  const chunksOfGas = 2;  // FIXME - unused
+  const txnsEstimate = 10;  // FIXME - unused
 
   beforeEach(() => {
     // Reset the mock before each test
@@ -42,16 +42,41 @@ describe('Pool creation with factory', () => {
     const objects = pool.objects;
     expect(objects.length).toBeGreaterThan(0);
   });
+});
 
-  it('splits a pool in correct-length pools', async () => {
-    // Create a pool
-    const pool: Pool = await Pool.full({
+
+describe.each([
+  [(_obj: SuiObjectRef | undefined) => true, "always true predicate"], 
+  [(_obj: SuiObjectRef | undefined) => false, "always false predicate"],
+  [(_obj: SuiObjectRef | undefined) => null, "always null predicate"], // FIXME - times out, infinite loop
+])(
+  '🌀 Pool splitting', (predicate: any, descr) => {
+  it(`splits a pool using an ${descr}`, async () => {
+    /* 
+    Create a pool 
+    */
+    const initial_pool: Pool = await Pool.full({
       keypair: adminKeypair,
       client: client,
     }); 
-    const objects = pool.objects;
-    const poolLength = objects.length;
+    const num_objects_before_split = initial_pool.objects.length;
+    
+    /* 
+    Split the initial pool, moving some objects to 
+    a newly created pool (depending on the predicate) 
+    */
+    const new_pool: Pool = initial_pool.split(predicate);  
+    const num_objects_new_pool = new_pool.objects.length;
 
-    expect(poolLength).toBeGreaterThan(0);
+    /* 
+    Number of objects in the initial pool has changed! 
+    Some of them have been moved to new_pool (based on the predicate), 
+    so we calculate the new number of objects in the initial pool. 
+    */
+    const num_objects_after_split = initial_pool.objects.length;
+
+    expect(num_objects_new_pool + num_objects_after_split)
+          .toEqual(num_objects_before_split);
   });
 });
+
