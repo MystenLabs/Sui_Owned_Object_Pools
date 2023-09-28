622e36affe84440f9667e6f27c3d35298011fab9
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2023-09-28 13:42:56 +0300

    Create pool.coin struct
    - Update unit tests
    
    - Update pool splitting so that
    a predicate is passed to determine
    how the pool's coins will get split.
    This was very similar to how
    the object splitting was implemented.
    
    TODO
    - Pool ownership of coins is yet to be
    defined.

--- src/pool.ts
+++ src/pool.ts
@@ -13,25 +13,31 @@ import {
   SuiObjectResponse,
 } from '@mysten/sui.js/src/types/objects';
 import { 
+  CoinStruct, 
+  PaginatedCoins } from '@mysten/sui.js/dist/cjs/client/types/';
+import { 
   SuiTransactionBlockResponseOptions, 
-  ExecuteTransactionRequestType
-} from '@mysten/sui.js/src/types/transactions';
+  ExecuteTransactionRequestType} from '@mysten/sui.js/src/types/transactions';
 import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { isValidSuiAddress } from "@mysten/sui.js/utils";
 
 
 export class Pool {
   private _keypair: Keypair;
   private _client: SuiClient;
   private _objects: SuiObjectRef[];
+  private _coins: CoinStruct[];
 
   private constructor(
     keypair: Keypair,
     client: SuiClient,
     objects: SuiObjectRef[],
+    coins: CoinStruct[],
   ) {
     this._keypair = keypair;
     this._client = client;
     this._objects = objects;
+    this._coins = coins;
   }
 
   static async full(input: { keypair: Keypair; client: SuiClient }) {
@@ -39,7 +45,8 @@ export class Pool {
     const owner = keypair.toSuiAddress();
 
     console.log('Creating Pool for account ', owner);
-
+    
+    // Get all objects owned by the pool's creator
     const objects: SuiObjectRef[] = [];
     let resp: PaginatedObjectsResponse | null;
     let cursor = null;
@@ -54,38 +61,100 @@ export class Pool {
       cursor = resp?.nextCursor;
     } while (resp.hasNextPage);
 
-    return new Pool(keypair, client, objects);
+    // Get all coins owned by the pool's creator
+    const coins: CoinStruct[] = [];
+    let coins_resp: PaginatedCoins | null;
+    cursor = null;
+    do {
+      coins_resp = await client.getAllCoins({ owner, cursor });
+      coins_resp.data.forEach((coin: CoinStruct) => {
+        coins.push(coin);
+      });
+      cursor = coins_resp?.nextCursor;
+    } while (coins_resp.hasNextPage);
+
+    return new Pool(keypair, client, objects, coins);
   }
 
   /**
-   * Split off a new Pool from this Pool using `pred` to select the objects
-   * that will end up in the new Pool.
+   * Split off a new Pool from this Pool using `pred_obj` to determine how
+   * the objects are split, and `pred_coins` to determine how the coins are.
    *
-   * `pred` is called on each object in the current pool, in turn.  If it
-   * returns `true`, then the object will be moved to the new Pool, if it
-   * returns `false`, then the object will stay in `this` Pool, and if it
-   * returns `null`, it skips all remaining objects and returns the split
-   * Pool immediately.
+   * A `pred_obj/coins` is called on each object in the current pool.objects and 
+   * pool.coins, in turn. 
+   * If the corresponding predicate: 
+   * 1. returns `true`, then the object will be moved to the new Pool, if it
+   * 2. returns `false`, then the object will stay in `this` Pool, and if it
+   * 3. returns `null`, it skips all remaining objects and returns the split Pool immediately.
+   * @param pred_obj a predicate function that returns true if an object 
+   * should be moved to the new pool, false if it should stay in the current pool,
+   * and null if the split should stop immediately.
+   * @param pred_coins a predicate function that returns true if a coin
+   * should be moved to the new's pool struct "coins", false if it should 
+   * stay in the current pool.coins.
    */
-  split(pred: (obj: SuiObjectRef | undefined) => boolean | null): Pool {
-    const keep: SuiObjectRef[] = [];
-    const give: SuiObjectRef[] = [];
+  split(pred_obj: (obj: SuiObjectRef | undefined) => boolean | null,
+        pred_coins: (obj: CoinStruct | undefined) => boolean | null): Pool {
+    const objects_to_give = this.split_objects(pred_obj);
+    const coins_to_give = this.split_coins(pred_coins);
 
-    outside:  // label to specify the null break statement
+    return new Pool(this._keypair, this._client, objects_to_give, coins_to_give);
+  }
+
+  /**
+   * Removes from the current pool and accumulates into an array the
+   * objects that will be moved to the new pool from the current pool.
+   * @param pred a predicate function that returns true if an object should
+   * be moved to the new pool after split
+   * @returns the array of objects that will be moved to the new pool
+   */
+  split_objects(pred: (obj: SuiObjectRef | undefined) => boolean | null): SuiObjectRef[] {
+    const objects_to_keep: SuiObjectRef[] = [];
+    const objects_to_give: SuiObjectRef[] = [];
+
+    outside:
     while (this._objects.length !== 0) {
       switch (pred(this._objects.at(-1))) {
         case true:
-          give.push(this._objects.pop()!);
+          objects_to_give.push(this._objects.pop()!);
           break;
         case false:
-          keep.push(this._objects.pop()!);
+          objects_to_keep.push(this._objects.pop()!);
           continue;
         case null:
           break outside;
       }
     }
-    this.objects.push(...keep);
-    return new Pool(this._keypair, this.client, give);
+    this.objects.push(...objects_to_keep);
+    return objects_to_give;
+  }
+
+  /**
+   * Removes from the current pool and accumulates into an array the 
+   * coins that will be moved to the new pool from the current pool. 
+   * @param pred a predicate function that returns true if a coin should 
+   * be moved to the new pool after split
+   * @returns the array of coins that will be moved to the new pool
+   */
+  split_coins(pred: (coin: CoinStruct | undefined) => boolean | null): CoinStruct[] {
+    const coins_to_keep: CoinStruct[] = [];
+    const coins_to_give: CoinStruct[] = [];
+
+    outside:
+    while (this._coins.length !== 0) {
+      switch (pred(this._coins.at(-1))) {
+        case true:
+          coins_to_give.push(this._coins.pop()!);
+          break;
+        case false:
+          coins_to_keep.push(this._coins.pop()!);
+          continue;
+        case null:
+          break outside;
+      }
+    }
+    this._coins.push(...coins_to_keep);
+    return coins_to_give;
   }
 
   async signAndExecuteTransactionBlock(input: {
@@ -123,10 +192,16 @@ export class Pool {
   public check_total_ownership(txb: TransactionBlock): boolean {
     const inputs = txb.blockData.inputs;
     return inputs.every((input) => {
-      // Skip the address input. Addresses are of type 'pure'.
-      // obviously it doesn't make sense to check if an address 
-      // is in the pool since it's not an object.
-      if (input.type! == 'pure') return true  
+      // Skip the signer's address - doesn't make sense to check for onwership
+      const is_address = isValidSuiAddress(input.value) && input.type! == 'pure';
+      if (is_address) return true  
+      
+      // Currently, we only check for object ownership.
+      // Coins are skipped - i.e. we pass them as true (owned).
+      // TODO: check for coin ownership
+      const is_coin = input.type! == 'pure';
+      if (is_coin) return true
+      
       // check that the object is in the pool
       return this.is_inside_pool(input.value)
     });
@@ -141,12 +216,16 @@ export class Pool {
     const object = this._objects.find(
       (obj) => obj.objectId === objectId
       );
-    const is_found = object !== undefined;
-    return is_found;
+      const is_found = object !== undefined;
+      return is_found;
   }
 
   get objects(): SuiObjectRef[] {
     return this._objects;
+  }
+
+  get coins(): CoinStruct[] {
+    return this._coins;
   }
 
   get client(): SuiClient {
--- test/unit/pool.test.ts
+++ test/unit/pool.test.ts
@@ -1,20 +1,16 @@
-import { getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
+import { CoinStruct, getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
 import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
 import { fromB64 } from '@mysten/sui.js/utils';
 import { SuiObjectRef } from '@mysten/sui.js/src/types/objects';
 
 import { Pool } from '../../src';
 import { TransactionBlock } from "@mysten/sui.js/transactions";
+import exp from 'constants';
 
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-// Test keys for address 0x8c94aaf11b8e3341d3b7b527daaa7b13e2637419db6bfad53b93d8d267ea8cb8
-const ADMIN_TEST_KEYS = [
-  'AMat/wSZ1kXntDIoMrcoLFB5nt2rY2qYU0ImLW5AsbZ6', // base64
-  '0xc6adff0499d645e7b4322832b7282c50799eddab636a985342262d6e40b1b67a', // hex
-  'flash leave dilemma swing lab flavor shoot civil rookie list gather soul', // mnemonic
-];
-
-const ADMIN_SECRET_KEY = ADMIN_TEST_KEYS[0];
+const ADMIN_SECRET_KEY: string = process.env.ADMIN_SECRET_KEY!;
 const adminPrivateKeyArray = Uint8Array.from(
   Array.from(fromB64(ADMIN_SECRET_KEY)),
 );
@@ -33,13 +29,16 @@ describe('Pool creation with factory', () => {
     jest.setTimeout(10000);
   });
 
+  /// WARNING this test might fail if the account
+  /// has no coins or objects (NFTs).
   it('creates a pool correctly', async () => {
     const pool: Pool = await Pool.full({
       keypair: adminKeypair,
       client: client,
     });
-    const objects = pool.objects;
-    expect(objects.length).toBeGreaterThan(0);
+
+    expect(pool.objects.length).toBeGreaterThan(0); 
+    expect(pool.coins.length).toBeGreaterThan(0);
   });
 });
 
@@ -54,25 +53,29 @@ describe('✂️ Pool splitting', () => {
       client: client,
     }); 
     const num_objects_before_split = initial_pool.objects.length;
-    
+    const num_coins_before_split = initial_pool.coins.length;
+
     /* 
     Split the initial pool, moving some objects to 
     a newly created pool. Since the predicate returns only true,
     all objects will be moved to the new pool.
     */
-    const always_true_predicate = (_obj: SuiObjectRef | undefined) => true;
-    const new_pool: Pool = initial_pool.split(always_true_predicate);  
+    const always_true_predicate = (_obj: SuiObjectRef | CoinStruct | undefined) => true;
+    const new_pool: Pool = initial_pool.split(always_true_predicate, always_true_predicate);  
     const num_objects_new_pool = new_pool.objects.length;
-
+    
     /* 
     Number of objects in the initial pool has changed! 
     Some of them have been moved to new_pool (based on the predicate), 
     so we calculate the new number of objects in the initial pool. 
     */
     const num_objects_after_split = initial_pool.objects.length;
+    const num_coins_after_split = initial_pool.coins.length;
 
     expect(num_objects_new_pool + num_objects_after_split)
           .toEqual(num_objects_before_split);
+    expect(num_coins_after_split + num_coins_before_split)
+          .toEqual(num_coins_before_split);    
   });
 
   it('splits a pool using an <always-false> predicate', async () => {
@@ -84,25 +87,28 @@ describe('✂️ Pool splitting', () => {
       client: client,
     }); 
     const num_objects_before_split = initial_pool.objects.length;
-    
+    const num_coins_before_split = initial_pool.coins.length;
+
     /* 
     Split the initial pool, moving some objects to 
     a newly created pool. Since the predicate returns only false,
     no objects will be moved to the new pool.
     */
-    const always_false_predicate = (_obj: SuiObjectRef | undefined) => false;
-    const new_pool: Pool = initial_pool.split(always_false_predicate);  
+    const always_false_predicate = (_obj: SuiObjectRef | CoinStruct | undefined) => false;
+    const new_pool: Pool = initial_pool.split(always_false_predicate, always_false_predicate);  
     const num_objects_new_pool = new_pool.objects.length;
-
+    const num_coins_new_pool = new_pool.coins.length;
    /* 
     Number of objects in the initial pool (could have) changed! 
     Some of them might have been moved to new_pool (based on the predicate), 
     so we calculate the new number of objects in the initial pool. 
     */
     const num_objects_after_split = initial_pool.objects.length;
-
+    const num_coins_after_split = initial_pool.coins.length;
     expect(num_objects_new_pool + num_objects_after_split)
           .toEqual(num_objects_before_split);
+    expect(num_coins_after_split + num_coins_new_pool)
+          .toEqual(num_coins_before_split);
   });
 
   it('splits a pool using an <always-null> predicate', async () => {
@@ -114,15 +120,17 @@ describe('✂️ Pool splitting', () => {
       client: client,
     }); 
     const num_objects_before_split = initial_pool.objects.length;
-    
+    const num_coins_before_split = initial_pool.coins.length;
+
     /* 
     Split the initial pool, moving some objects to 
     a newly created pool. Since the predicate returns only null,
     no objects will be moved to the new pool.
     */
-    const always_null_predicate = (_obj: SuiObjectRef | undefined) => null;
-    const new_pool: Pool = initial_pool.split(always_null_predicate);  
+    const always_null_predicate = (_obj: SuiObjectRef | CoinStruct | undefined) => null;
+    const new_pool: Pool = initial_pool.split(always_null_predicate, always_null_predicate);  
     const num_objects_new_pool = new_pool.objects.length;
+    const num_coins_new_pool = new_pool.coins.length;
 
     /* 
     Number of objects in the initial pool (could have) changed! 
@@ -130,13 +138,20 @@ describe('✂️ Pool splitting', () => {
     so we calculate the new number of objects in the initial pool. 
     */
     const num_objects_after_split = initial_pool.objects.length;
+    const num_coins_after_split = initial_pool.coins.length;
 
     expect(num_objects_new_pool + num_objects_after_split)
           .toEqual(num_objects_before_split);
+    expect(num_coins_after_split + num_coins_new_pool)
+          .toEqual(num_coins_before_split);
   });
 
   /// This is not a testing an edge case scenario.
   /// In this case we use a predicate that could be used in a real scenario.
+  /// === WARNING! === 
+  /// To run this test you need to: 
+  /// 1. have at least N objects in your account.
+  /// 2. have at least C coins in your account.
   it('splits a pool using a normal-scenario predicate', async () => {
     // Create a pool 
     const initial_pool: Pool = await Pool.full({
@@ -144,15 +159,33 @@ describe('✂️ Pool splitting', () => {
       client: client,
     }); 
     const num_objects_before_split = initial_pool.objects.length;
-    
+    const num_coins_before_split = initial_pool.coins.length;
+
     /*
     Define a normal scenario predicate.
     Transfer 10 objects to the new pool and keep the rest to the initial pool.
     */
+
+    // Check that N is less than the number of objects in the initial pool just to be safe
+    const num_objects_for_new_pool = 3;  // i.e. N (current number of objects in pool'creators account)
+    expect(num_objects_for_new_pool).toBeLessThanOrEqual(num_objects_before_split);
+    // Define the predicate for the pool.objects split
     var counter = 0;
-    const predicate = (obj: SuiObjectRef | undefined): boolean | null => {
-      if (counter < 10) {
+    const predicate_obj = (obj: SuiObjectRef | undefined): boolean | null => {
+      if (counter < num_objects_for_new_pool) {
         counter++;
+        return true;
+      } else {
+        return false;
+      }
+    } ;
+
+    // Define the predicate for the pool.coins split
+    const num_coins_for_new_pool = 1;  // i.e. N (current number of objects in pool'creators account)
+    let counter_coins = 0;
+    const predicate_coins = (_coin: CoinStruct | undefined): boolean | null => {
+      if (counter_coins < num_coins_for_new_pool) {
+        counter_coins++;
         return true;
       } else {
         return false;
@@ -163,19 +196,26 @@ describe('✂️ Pool splitting', () => {
     Split the initial pool, moving some objects to 
     a newly created pool.
     */
-    const new_pool: Pool = initial_pool.split(predicate);  
+    const new_pool: Pool = initial_pool.split(predicate_obj, predicate_coins);  
     const num_objects_new_pool = new_pool.objects.length;
-
+    const num_coins_new_pool = new_pool.coins.length;
     /* 
     Number of objects in the initial pool has changed! 
     Some of them have been moved to new_pool (based on the predicate), 
     so we calculate the new number of objects in the initial pool. 
     */
     const num_objects_after_split = initial_pool.objects.length;
+    const num_coins_after_split = initial_pool.coins.length;
 
-    expect(num_objects_new_pool).toEqual(10);
-    expect(num_objects_after_split).toEqual(num_objects_before_split - 10);
+    // Validity checks for object array splitting
+    expect(num_objects_new_pool).toEqual(num_objects_for_new_pool);
+    expect(num_objects_after_split).toEqual(num_objects_before_split - num_objects_for_new_pool);
     expect(num_objects_new_pool + num_objects_after_split).toEqual(num_objects_before_split);
+    
+    // Validity checks for coin array splitting
+    expect(num_coins_new_pool).toEqual(num_coins_for_new_pool);
+    expect(num_coins_after_split).toEqual(num_coins_before_split - num_coins_for_new_pool);
+    expect(num_coins_new_pool + num_coins_after_split).toEqual(num_coins_before_split);
   });
 });
 
@@ -183,7 +223,7 @@ describe('✂️ Pool splitting', () => {
 describe('🌊 Basic flow of sign & execute tx block', () => {
   const chunksOfGas = 2;  // FIXME - unused
   const txnsEstimate = 10;  // FIXME - unused
-  const testObjectId = "0x295181920738375f18017665d05d976aa5b8effa3c9ec79c09aa71f39f2619c9"; // TODO read it from dotenv
+  const testObjectId = process.env.TEST_NFT_OBJECT_ID!;
 
   beforeEach(() => {
     // Reset the mock before each test
@@ -205,7 +245,13 @@ describe('🌊 Basic flow of sign & execute tx block', () => {
     // Admin transfers an object that belongs to him back to himself.  
     const txb = new TransactionBlock();
     const adminAddress = adminKeypair.getPublicKey().toSuiAddress();
+    
+    // Include a transfer nft object transaction in the transaction block
     txb.transferObjects([txb.object(testObjectId)], txb.pure(adminAddress))
+    
+    // Include a transfer coin transaction in the transaction block
+    const [coin] = txb.splitCoins(txb.gas, [txb.pure(1)]);
+    txb.transferObjects([coin], txb.pure("0xCAFE")); // Transferring the object to a test address
     
     // Check ownership of the objects in the transaction block.
     expect(pool.check_total_ownership(txb)).toBeTruthy();
@@ -224,9 +270,9 @@ describe('🌊 Basic flow of sign & execute tx block', () => {
 
     // Admin transfers a random object that doesn't belong to himself.  
     const txb = new TransactionBlock();
-    const falsyObjectId = "0x0287a708d359616a50d561d86aeda23c85d3b707fb18574ee35fc0f8c7518219"; // random object id - non existent
+    const falsyObjectId = "0x02004"; // random object id - non existent
     const adminAddress = adminKeypair.getPublicKey().toSuiAddress();
-    txb.transferObjects([txb.object(falsyObjectId)], txb.pure(adminAddress))
+    txb.transferObjects([txb.object(falsyObjectId)], txb.pure(adminAddress));
 
     // Check ownership of the objects in the transaction block.
     expect(pool.check_total_ownership(txb)).toBeFalsy();
@@ -245,7 +291,6 @@ describe('🌊 Basic flow of sign & execute tx block', () => {
 
     // Admin transfers an object that belongs to him back to himself.  
     const txb = new TransactionBlock();
-    const testObjectId = "0x295181920738375f18017665d05d976aa5b8effa3c9ec79c09aa71f39f2619c9"; // TODO read it from dotenv
     const adminAddress = adminKeypair.getPublicKey().toSuiAddress();
     txb.transferObjects([txb.object(testObjectId)], txb.pure(adminAddress))
     
