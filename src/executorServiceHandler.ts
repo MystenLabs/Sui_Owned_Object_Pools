fe59569d6d789b3b032aa70b6d06dcb898a288b5
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2023-10-19 19:00:14 +0300

    Implement basic flow
    Include a test scenario with only one request.
    
    TODO:
    - Add a new test for the pool to handle more
    incoming requests and observe the flow

--- src/executorServiceHandler.ts
+++ src/executorServiceHandler.ts
@@ -1,59 +1,122 @@
-import { Pool } from './pool'
-import { TransactionBlock } from '@mysten/sui.js/transactions';
-import { SuiClient } from '@mysten/sui.js/client';
+import { SuiClient, CoinStruct } from '@mysten/sui.js/client';
 import { Keypair } from '@mysten/sui.js/src/cryptography';
+import { SuiObjectRef } from '@mysten/sui.js/src/types/objects';
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+
+import { Pool } from './pool';
 
 type WorkerPool = {
-  status: "available" | "busy"
-  pool: Pool
-}
+  status: 'available' | 'busy';
+  pool: Pool;
+};
+
+// SplitStrategy defines the predicates used to split the pool's objects and coins
+// to get new worker pools.
+type SplitStrategy = {
+  objPred: (obj: SuiObjectRef | undefined) => boolean | null;
+  coinPred: (coin: CoinStruct | undefined) => boolean | null;
+};
+
 export class ExecutorServiceHandler {
-  private _keypair: Keypair;
   private _mainPool: Pool;
   private _workers: WorkerPool[] = [];
-  private constructor(keypair: Keypair, mainPool: Pool) {
-    this._keypair = keypair;
+  private constructor(mainPool: Pool) {
     this._mainPool = mainPool;
   }
 
   public static initialize(keypair: Keypair, client: SuiClient) {
-    return Pool.full({keypair: keypair, client}).then((pool) => {
-      return new ExecutorServiceHandler(keypair, pool);
+    return Pool.full({ keypair: keypair, client }).then((pool) => {
+      return new ExecutorServiceHandler(pool);
     });
   }
 
-  async execute(txb: TransactionBlock, client: SuiClient){
-    const worker: WorkerPool | undefined = this.getWorker();
-    if (worker === undefined) {
-      // TODO Add new worker(s) to the pool by splitting the main pool.
+  public async execute(
+    txb: TransactionBlock,
+    client: SuiClient,
+    splitStrategy: SplitStrategy,
+    retries: number = 1,
+  ) {
+    let res;
+    do {
+      res = await this.executeFlow(txb, client, splitStrategy);
+      if (res) {
+        return res;
+      }
+    } while (retries-- > 0);
+    throw new Error('Internal server error - could not execute the txb');
+  }
+
+  private async executeFlow(
+    txb: TransactionBlock,
+    client: SuiClient,
+    splitStrategy: SplitStrategy,
+  ) {
+    const worker: WorkerPool | undefined = this.getAWorker();
+    const noWorkerAvailable = worker === undefined;
+    if (noWorkerAvailable) {
+      this.addWorker(splitStrategy);
+      return
     } else {
-      worker.status = "busy";
-      const result = await worker.pool.signAndExecuteTransactionBlock(
-        { transactionBlock: txb, client: client }
-      )
-      worker.status = "available";
-      return result
+      // An available worker is found! Assign to it the task of executing the txb.
+      worker.status = 'busy';  // Worker is now busy
+
+      // TODO - catch dry-run fail error, remove worker, retrieve another worker and retry the execution
+      const result = await worker.pool.signAndExecuteTransactionBlock({
+        transactionBlock: txb,
+        client: client,
+      });
+
+      worker.status = 'available';  // Execution finished, the worker is now available again.
+      return result;
     }
   }
 
   /*
-  Get an available worker.
-  If a worker is not found in the span of TIMEOUT_MS, return undefined.
+  Get an available worker from the workers array.
+  If an available worker is not found in the time span of TIMEOUT_MS, return undefined.
   */
-  private getWorker(): WorkerPool | undefined {
+  private getAWorker(): WorkerPool | undefined {
     const TIMEOUT_MS = 1000;
     const startTime = new Date().getTime();
     while (new Date().getTime() - startTime < TIMEOUT_MS) {
       const result = this._workers.find(
-        (worker: WorkerPool) => worker.status === "available"
+        (worker: WorkerPool) => worker.status === 'available',
       );
       if (result) {
-          console.log('Available worker found!');
-          return result;
+        console.log('Available worker found!');
+        return result;
       }
     }
     if (new Date().getTime() - startTime >= TIMEOUT_MS) {
       console.log('Timeout reached - no available worker found.');
+    }
+  }
+
+  /*
+    Add a worker to the workers array.
+    The worker is created by splitting the main pool and the new pool
+    that is produced is added to the workers array.
+   */
+  private addWorker(splitStrategy: SplitStrategy) {
+    const newPool = this._mainPool.split(
+      splitStrategy.objPred,
+      splitStrategy.coinPred,
+    );
+    this._workers.push({ status: 'available', pool: newPool });
+  }
+
+  /*
+   Remove the worker from the workers array and merge
+   it back to the main pool.
+   TODO - do this if the pool.signAndExecuteTransactionBlock *dry run* fails.
+  */
+  private removeWorker(worker: WorkerPool) {
+    const index = this._workers.indexOf(worker);
+    if (index > -1) {
+      this._workers.splice(index, 1);
+      this._mainPool.merge(worker.pool);
+    } else {
+      throw new Error('Worker not found in workers array.');
     }
   }
 }
--- test/unit/executorServiceHandler.test.ts
+++ test/unit/executorServiceHandler.test.ts
@@ -3,6 +3,7 @@ import { SuiClient } from '@mysten/sui.js/client';
 import { fromB64 } from '@mysten/sui.js/utils';
 import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
 import { TransactionBlock } from '@mysten/sui.js/transactions';
+import { SuiObjectRef } from '@mysten/sui.js/src/types/objects';
 
 
 const path = require('path');
@@ -22,8 +23,26 @@ const client = new SuiClient({
 
 describe('Test pool adaptability to requests with ExecutorServiceHandler', () => {
   it('executes a txb', async () => {
-    const eshandler = await ExecutorServiceHandler.initialize(adminKeypair, client);
+    // TODO - Create multiple txbs like this in order to stress test the eshandler
+    // Create a transaction that transfers MIST from the admin to a test user address.
     const txb = new TransactionBlock();
-    eshandler.execute(txb, client);
+    const MIST_TO_TRANSFER = 1000;
+    const [coin] = txb.splitCoins(txb.gas, [txb.pure(MIST_TO_TRANSFER)]);
+    txb.transferObjects([coin], txb.pure(process.env.TEST_USER_ADDRESS!));
+
+    // Pass this transaction to the ExecutorServiceHandler. The ExecutorServiceHandler will
+    // forward the transaction to a worker pool, which will sign and execute the transaction.
+    const eshandler = await ExecutorServiceHandler.initialize(adminKeypair, client);
+    var firstRunFlag = 0;
+    const splitStrategy = {
+      objPred: (_: SuiObjectRef | undefined) => null,
+
+      // Only send the first coin to the new split pool. Keep the rest
+      coinPred: () => {
+        return firstRunFlag++ === 0
+      },
+    }
+    const res = await eshandler.execute(txb, client, splitStrategy);
+    expect(res.effects!.status.status).toEqual('success');
   });
 });
