44585cea2fa35690e10af43f15981a81cd3d6890
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2023-10-23 16:58:39 +0300

    Create SetupTestsHelper.setupAdmin

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,14 @@
+import path from 'path';
 import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
 import { fromB64 } from '@mysten/sui.js/utils';
+import { SuiClient } from '@mysten/sui.js/client';
+import { SuiObjectResponse } from '@mysten/sui.js/src/client/types/generated';
+import { Coin } from '@mysten/sui.js/dist/cjs/framework/framework';
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+
+require('dotenv').config({
+  path: path.resolve(__dirname, '../test/.env'),
+});
 
 /// Method to make keypair from private key that is in string format
 export function getKeyPair(privateKey: string): Ed25519Keypair {
@@ -21,4 +30,113 @@ export function compareMaps<T>(map1: Map<string, T>, map2: Map<string, T>) {
     }
   }
   return true;
+}
+
+export class SetupTestsHelper {
+  public MINIMUM_ADMIN_COINS_NEEDED = 4;
+  public MINIMUM_COIN_BALANCE = 20000000;
+
+  private client: SuiClient;
+  private adminKeypair: Ed25519Keypair;
+
+  constructor() {
+    this.client = new SuiClient({
+      url: process.env.SUI_NODE!,
+    });
+    this.adminKeypair = getKeyPair(process.env.ADMIN_SECRET_KEY!);
+  }
+
+  /*
+  Reassure that the admin has enough coins and objects to run the tests
+   */
+  public async setupAdmin() {
+    const owner = this.adminKeypair.toSuiAddress();
+    let cursor: string | null | undefined = null;
+    let resp;
+
+    // Parse objects and coins to separate arrays
+    const objects: SuiObjectResponse[] = [];
+    const suiCoins: SuiObjectResponse[] = [];
+    do {
+      resp = await this.client.getOwnedObjects({
+        owner,
+        options: {
+          showContent: true,
+          showType: true,
+        },
+        cursor,
+      });
+      resp?.data.forEach((object) => {
+        if (Coin.isSUI(object)) {
+          suiCoins.push(object);
+        } else {
+          objects.push(object);
+        }
+      });
+      cursor = resp?.nextCursor;
+    } while (resp?.hasNextPage);
+
+    // If the admin has not enough coins, add more
+    let coinToSplit: SuiObjectResponse;
+    if (suiCoins.length < this.MINIMUM_ADMIN_COINS_NEEDED) {
+      for (
+        let i = 0;
+        i < this.MINIMUM_ADMIN_COINS_NEEDED - suiCoins.length;
+        i++
+      ) {
+        coinToSplit = suiCoins.find((coin) =>
+          Coin.getBalance(coin)
+            ? Coin.getBalance(coin)! > 2 * this.MINIMUM_COIN_BALANCE
+            : false,
+        )!;
+        const gasCoin = suiCoins.find((coin) =>
+          coinToSplit?.data?.objectId !== coin.data?.objectId
+        );
+        if (!gasCoin) {
+          throw new Error('Failed to find a coin to use as gas. Split a coin manually or get one from faucet.');
+        }
+        if (coinToSplit) {
+          await this.addNewCoinToAccount(coinToSplit, gasCoin);
+        }
+      }
+    }
+  }
+
+  /*
+  Increase the coins of the admin account
+   */
+  private async addNewCoinToAccount(fromCoin: SuiObjectResponse, gasCoin: SuiObjectResponse) {
+    const transactionBlockSplitCoin = new TransactionBlock();
+    const [coin] = transactionBlockSplitCoin.splitCoins(
+      transactionBlockSplitCoin.pure(fromCoin.data?.objectId!),
+      [transactionBlockSplitCoin.pure(this.MINIMUM_COIN_BALANCE)],
+    );
+    transactionBlockSplitCoin.transferObjects(
+      [coin],
+      transactionBlockSplitCoin.pure(this.adminKeypair.toSuiAddress()),
+    );
+    transactionBlockSplitCoin.setGasBudget(10000000);
+
+    transactionBlockSplitCoin.setGasPayment([{
+        objectId: gasCoin.data?.objectId!,
+        digest: gasCoin.data?.digest!,
+        version: gasCoin.data?.version!,
+    }]);
+
+    let coins = await this.client.getAllCoins({
+      owner: this.adminKeypair.toSuiAddress(),
+    });
+
+    const res = await this.client.signAndExecuteTransactionBlock({
+      // @ts-ignore
+      transactionBlock: transactionBlockSplitCoin,
+      requestType: 'WaitForLocalExecution',
+      options: { showEffects: true },
+      signer: this.adminKeypair,
+    });
+
+    if (res.effects?.status?.status !== 'success') {
+      throw new Error('Failed to split coin');
+    }
+  }
 }
