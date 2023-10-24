3155ef0c93cde5ec87a16a8de10c45128c256e77
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2023-10-24 15:54:11 +0300

    Add setup step: reassure that admin has at least 2 coins
    also fix TEST_USER_ADDRESS y/n step

--- test/initial_setup.sh
+++ test/initial_setup.sh
@@ -10,6 +10,15 @@ else
     ../move_examples/setup/publish.sh
 fi
 
+read -p "Reassure that Admin has at least 2 to make transactions? - a random coin will be split into 2 (y/n): " response
+response=$(echo "$response" | tr '[:upper:]' '[:lower:]') # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]; then
+    # Split a coin of the AdminAccount (i.e. the current active-address) so that you have at least 2 coins.
+    COIN=$(sui client objects "$(sui client active-address)" --json | jq -r '.[] | select(.data.content.type == "0x2::coin::Coin<0x2::sui::SUI>") | .data.objectId' | head -n 1)
+    sui client split-coin --coin-id $COIN --amounts 1000000000 --gas-budget 1000000000
+    echo "Done!"
+fi
+
 read -p "Create a new TEST_USER_ADDRESS to $(sui client active-env)? (y/n): " response
 response=$(echo "$response" | tr '[:upper:]' '[:lower:]') # tolower
 if ! [[ "$response" =~ ^(yes|y)$ ]]; then
@@ -24,5 +33,5 @@ TEST_USER_ENV
 
 SUI_AMOUNT=2000000000
 echo "Sending $SUI_AMOUNT to TEST_USER_ADDRESS: $TEST_USER_ADDRESS"
-COIN=$(sui client objects $(sui client active-address) --json | jq -r '.[] | select(.data.content.type == "0x2::coin::Coin<0x2::sui::SUI>") | .data.objectId' | head -n 1)
+COIN=$(sui client objects "$(sui client active-address)" --json | jq -r '.[] | select(.data.content.type == "0x2::coin::Coin<0x2::sui::SUI>") | .data.objectId' | head -n 1)
 sui client pay --recipients $TEST_USER_ADDRESS --amounts $SUI_AMOUNT --gas-budget 10000000 --input-coins $COIN
