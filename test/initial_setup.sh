9a44aeb63f45c367a073c8c7b7a61552963a2577
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2023-10-24 15:09:07 +0300

    Transfer some sui to the test user when creating him

--- test/initial_setup.sh
+++ test/initial_setup.sh
@@ -21,3 +21,8 @@ NEW_ADDRESS_JSON=$(sui client new-address ed25519 --json)
 cat >>.env <<-TEST_USER_ENV
 TEST_USER_ADDRESS=$(echo "$NEW_ADDRESS_JSON" | jq -r '.address')
 TEST_USER_ENV
+
+SUI_AMOUNT=2000000000
+echo "Sending $SUI_AMOUNT to TEST_USER_ADDRESS: $TEST_USER_ADDRESS"
+COIN=$(sui client objects $(sui client active-address) --json | jq -r '.[] | select(.data.content.type == "0x2::coin::Coin<0x2::sui::SUI>") | .data.objectId' | head -n 1)
+sui client pay --recipients $TEST_USER_ADDRESS --amounts $SUI_AMOUNT --gas-budget 10000000 --input-coins $COIN
