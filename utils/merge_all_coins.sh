69a9f56942b7af27e198df703e1fb1adca98d249
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2023-10-24 11:34:55 +0300

    Refactor and parse ADMIN_ADDRESS from environment

--- utils/merge_all_coins.sh
+++ utils/merge_all_coins.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# Run the command and store the output in a variable
-input_data=$(sui client objects 0x1dbd03ee8b78f826ddecedbd1295feb51eb7029b158f5be408f4cd232117ac36 | grep GasCoin)
+# Get current admin's gascoins info
+gascoins_info=$(sui client objects $ADMIN_ADDRESS | grep GasCoin)
 
-# Extracting coinIds and constructing the command
+# Extracting coinIds from the gascoin info
 coinIds=()
 while read -r line; do
     coinId=$(echo "$line" | awk '{print $1}')
     coinIds+=("$coinId")
-done <<< "$input_data"
+done <<< "$gascoins_info"
 
-# Constructing the command with the coinIds array
+# Merge all gas coins with the first one in the list
 for ((i = 1; i < ${#coinIds[@]} ; i++)); do
-    command="sui client merge-coin --primary-coin ${coinIds[0]} --coin-to-merge ${coinIds[i]} --gas-budget 10000000"
+    command="sui client merge-coin \
+        --primary-coin ${coinIds[0]} \
+        --coin-to-merge ${coinIds[i]} \
+        --gas-budget 10000000"
     echo "Executing command: $command"
     eval "$command"
 done
-
-sui client objects 0x1dbd03ee8b78f826ddecedbd1295feb51eb7029b158f5be408f4cd232117ac36 | grep GasCoin
