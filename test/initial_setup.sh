8bc0cb19dfe9e718c0087b19ea6da3980ce0d2ee
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2023-10-24 16:06:37 +0300

    Move test user creation inside if block

--- test/initial_setup.sh
+++ test/initial_setup.sh
@@ -25,17 +25,11 @@ fi
 
 read -p "Create a new TEST_USER_ADDRESS to $(sui client active-env)? (y/n): " response
 response=$(echo "$response" | tr '[:upper:]' '[:lower:]') # tolower
-if ! [[ "$response" =~ ^(yes|y)$ ]]; then
-    echo "Not creating TEST_USER_ADDRESS"
-    exit 1
+if [[ "$response" =~ ^(yes|y)$ ]]; then
+    NEW_ADDRESS_JSON=$(sui client new-address ed25519 --json)
+    cat "TEST_USER_ADDRESS=$(echo "$NEW_ADDRESS_JSON" | jq -r '.address')">>.env
+    SUI_AMOUNT=2000000000
+    echo "Sending $SUI_AMOUNT to TEST_USER_ADDRESS: $TEST_USER_ADDRESS"
+    COIN=$(sui client objects "$(sui client active-address)" --json | jq -r '.[] | select(.data.content.type == "0x2::coin::Coin<0x2::sui::SUI>") | .data.objectId' | head -n 1)
+    sui client pay --recipients $TEST_USER_ADDRESS --amounts $SUI_AMOUNT --gas-budget 10000000 --input-coins $COIN
 fi
-
-NEW_ADDRESS_JSON=$(sui client new-address ed25519 --json)
-cat >>.env <<-TEST_USER_ENV
-TEST_USER_ADDRESS=$(echo "$NEW_ADDRESS_JSON" | jq -r '.address')
-TEST_USER_ENV
-
-SUI_AMOUNT=2000000000
-echo "Sending $SUI_AMOUNT to TEST_USER_ADDRESS: $TEST_USER_ADDRESS"
-COIN=$(sui client objects "$(sui client active-address)" --json | jq -r '.[] | select(.data.content.type == "0x2::coin::Coin<0x2::sui::SUI>") | .data.objectId' | head -n 1)
-sui client pay --recipients $TEST_USER_ADDRESS --amounts $SUI_AMOUNT --gas-budget 10000000 --input-coins $COIN
